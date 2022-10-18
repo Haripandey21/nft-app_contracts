abbed68908048572a1cb9154bd9cb5bfe75e74bd
haripandey21 <iamgoing1214@gmail.com>
2022-10-19 02:01:02 +0800

    contract creation and testing completed

---
+++ scripts/1_Nft_main.js
@@ -0,0 +1,16 @@
+const { ethers } = require("hardhat");
+
+async function main() {
+  const ERC721Contracts = await ethers.getContractFactory("Erc20_unlimited");
+  const deployedERC721Contracts  = await ERC721Contracts.deploy();
+  await deployedERC721Contracts.deployed();
+  
+  console.log(
+    "deployed Contract Address:",deployedERC721Contracts.address );
+}
+main()
+ .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+});
---
+++ test/minitng_methodTest.js
@@ -0,0 +1,19 @@
+const { expect } = require("chai");
+const hre = require("hardhat");
+let ERC721Contracts
+
+describe("deployment testing...", function () {
+  beforeEach(async () => {
+    [addr1, addr2, addr3] = await hre.ethers.getSigners();
+     ERC721Contracts = await ethers.getContractFactory("Nft_main");
+    deployedERC721Contracts = await ERC721Contracts.deploy();
+    await deployedERC721Contracts.deployed();
+  });
+  it("minting testing ... ", async () => {
+ 
+    await deployedERC721Contracts.safeMint("Nft_1");
+   let owner=await deployedERC721Contracts.ownerOf(1)
+   expect (addr1.address).to.equal(owner)
+   //console.log(owner)
+  });
+})
